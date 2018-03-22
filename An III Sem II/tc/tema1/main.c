5291bf55c6f9bbea970b44f42c3d60ac02c930f8
Ionut Calofir <calofir.ionut@yahoo.com>
2018-03-22 18:55:41 +0200

    Add tema 1 for tc

---
+++ An III Sem II/tc/tema1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/* 
+ * Function for testing flex
+ */
+void test_flex() {
+  int arr[22];
+  int *p;
+  float b = 5.72;
+  
+  int i = 0;
+  for (i = 0; i <= 10; ++i) {
+    if (i >= 5) {
+      continue;
+    }
+  }
+  while (true) {
+    break;
+  }
+}
+
+int main() {
+  test_flex()
+
+  // print Hello, World!
+  printf("Hello, World!");
+  return 0;
+}
